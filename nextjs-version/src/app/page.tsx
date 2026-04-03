dd8b7a1ae4a09866932a303381651ed63ace9cec
cmy-nl <mgrotenhuis@cmy.nl>
2026-04-04 00:01:40 +0200

    Update page.tsx

--- nextjs-version/src/app/page.tsx
+++ nextjs-version/src/app/page.tsx
@@ -1,5 +1,5 @@
-import LandingPage from "@/app/landing/page";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  return <LandingPage />;
+  redirect("/landing");
 }
